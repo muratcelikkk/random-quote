1b844dfe739027dced37ab1a44decd37b3eac979
murat <murat.tc2014@gmail.com>
2023-04-04 22:31:46 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [randomIndex, setRandomIndex] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("https://type.fit/api/quotes");
+        setQuotes(response.data);
+      } catch (error) {
+        console.log("Error fetching data", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleClick = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setRandomIndex(randomIndex);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="quote-container">
+      {quotes[randomIndex] && (
+        <div className="quote">
+          <p>{quotes[randomIndex].text}</p>
+          <p className="author">{quotes[randomIndex].author}</p>
+        </div>
+      )}
+      <div className="btn-container">
+        <button className="btn" onClick={handleClick}>
+          Random Quote
+        </button>
+      </div>
     </div>
   );
 }
